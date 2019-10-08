a46994a08f90e39457cd92f128b6f64b2bb827d1
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2019-10-08 10:24:47 -0500

    Add some input validation

--- scripts/get_extension_requirements.py
+++ scripts/get_extension_requirements.py
@@ -1,4 +1,7 @@
 import argparse
+import os
+import sys
+import subprocess
 
 parser = argparse.ArgumentParser("Tool to get the instruction extensions required for a given program.")
 
@@ -7,9 +10,21 @@ parser.add_argument("-d", "--definitions", help="The file containing instruction
 
 args = parser.parse_args()
 
+if not os.path.isfile(args.input):
+    print(f"Input file {args.input} doesn't exist or is a directory!")
+    sys.exit(0)
+
+if not os.path.isfile(args.definitions):
+    print(f"Definitions file {args.definitions} doesn't exist or is a directory!")
+    sys.exit(0)
+
+input_file = args.input
+definitions_file = args.definitions
+
 # We need to find an appropriate dissassembler
 disassembler = None
 
+print(subprocess.check_output(['which', 'objdump']))
 
 
-print(f"File to inspect: {args.input}")
+print(f"File to inspect: {input_file}")
