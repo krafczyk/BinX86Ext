1efc3dacb747a9bc2589d0ccfa60aad0c9fe5ba3
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2019-10-08 10:03:17 -0500

    Add option to define instruction data

--- scripts/get_extension_requirements.py
+++ scripts/get_extension_requirements.py
@@ -3,7 +3,13 @@ import argparse
 parser = argparse.ArgumentParser("Tool to get the instruction extensions required for a given program.")
 
 parser.add_argument("-i", "--input", help="The binary file to inspect", type=str, required=True)
+parser.add_argument("-d", "--definitions", help="The file containing instruction definitions. Should be a .csv file", default="instructions.csv")
 
 args = parser.parse_args()
+
+# We need to find an appropriate dissassembler
+disassembler = None
+
+
 
 print(f"File to inspect: {args.input}")
